9b2a0d77d812ec2cbc5e63c5392319c8961ec2d4
gjescuderoamodeo <gjescuderoamodeo@gmail.com>
2023-03-07 11:07:34 +0100

    gestionRutasExamen

---
+++ gestionRutas_GuillermoJoseEscuderoAmodeo/src/main/java/com/mycompany/gestionrutas_guillermojoseescuderoamodeo/GestionRutas_GuillermoJoseEscuderoAmodeo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gestionrutas_guillermojoseescuderoamodeo;
+
+/**
+ *
+ * @author GuillermoJ
+ */
+public class GestionRutas_GuillermoJoseEscuderoAmodeo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
